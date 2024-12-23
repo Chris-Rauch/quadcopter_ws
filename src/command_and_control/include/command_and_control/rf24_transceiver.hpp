c20b4f5fb973751e545761a3f60dbef7eb4d8025
Chris-Rauch <rauch.christopher13@gmail.com>
2024-12-22 20:20:53 -0800

    "Integrated RF24 driver code into ws"

--- src/command_and_control/include/command_and_control/rf24_transceiver.hpp
+++ src/command_and_control/include/command_and_control/rf24_transceiver.hpp
@@ -1,11 +1,25 @@
+#define CE_PIN 22
+#define CSN_PIN 0
+#define IRQ_PIN 8
+
 #ifndef RF24_TRANSCEIVER_HPP
 #define RF24_TRANSCEIVER_HPP
 
+#include <cstdlib>
 #include "rclcpp/rclcpp.h"
 #include "std_msgs/msg/String"
+#include "/home/chris/rf24libs/RF24/RF24.h"
+
+const int SPI_SPEED = 2000000;
+const char* PIPE[2] = {"1Node", "2Node"};
+const int PAYLOAD_SIZE = 32;
 
 namespace command_and_control {
-
+  /**
+   * @brief The transceiver is configured to always be listening, except when
+   * transmitting.
+   * 
+   */
   class RF24Transceiver : public rclcpp::Node
   {
   public:
@@ -19,12 +33,14 @@ namespace command_and_control {
     void transmit_command(const std::shared_ptr<interfaces::srv::MotorCommand::Request> request,
         std::shared_ptr<interfaces::srv::MotorCommand::Response> response);
 
-    // set up telemetry publisher 
-    //telem_publisher_ = this->create_publisher<interfaces::msg::TelemetryMsg>("telemetry", 10);
-    
+  private:
+    void _receiverCallback(); 
+    void _configureListenMode();
+    void _configureWriteMode();
   private:
     rclcpp::Publisher<interfaces::msg::TelemetryMsg>::SharedPtr telem_publisher_;
-  }
+    RF24 radio_;
+  };
 
 } // namespace command_and_control
 
--- src/command_and_control/src/rf24_transceiver.cpp
+++ src/command_and_control/src/rf24_transceiver.cpp
@@ -1,12 +1,49 @@
 #include "command_and_control/rf24_transceiver.cpp"
 
 namespace command_and_control {
+  
   RF24Transceiver::RF24Transceiver()
   {
-    // I NEED THE CODE ON MY WINDOWS MACHINE HERE
+    radio_ = RF24(CE_PIN, CSN_PIN, SPI_SPEED); // Initialize RF24
+    while (!radio_.begin()) {}                // Wait until RF24 is ready
+    radio_.setPALevel(RF24_PA_MIN);           // Set power level
+    radio_.setDataRate(RF24_250KBPS);         // Set data rate
+    radio_.setPayloadSize(PAYLOAD_SIZE);      // Set payload size
+    radio_.openReadingPipe(0, PIPE[0]);       // Open pipe 0 for reading
+    radio_.openWritingPipe(PIPE[1]);          // Open pipe 1 for writing
+    radio_.startListening();                  // Start listening mode
+    radio_.setChannel(69);                    // Set RF24 channel
+    
+    attachInterrupt(digitalPinToInterrupt(IRQ_PIN), _receiverCallback, FALLING); // Attach IRQ callback
 
-    // init telem publisher
-    telem_publisher_ = this->create_publisher<interfaces::msg::TelemetryMsg>("telemetry", 10);
+    telem_publisher_ = this->create_publisher<interfaces::msg::TelemetryMsg>("telemetry", 10); // Init ROS2 publisher
+  }
+
+
+  void RF24Transceiver::transmit_command(const std::shared_ptr<interfaces::srv::MotorCommand::Request> request,
+    std::shared_ptr<interfaces::srv::MotorCommand::Response> response)
+  {
+    uint8_t payload = request->payload;
+
+    radio_.stopListening(); // Switch to transmit mode
+    response->ack = radio.write(payload, PAYLOAD_SIZE); // Send data and store acknowledgment
+    radio_.startListening(); // Switch back to listening mode
+  }
+
+  
+
+  void RF24Transceiver::_receiverCallback()
+  {
+    uint8_t payload[32];
+    
+    // while data is being sent, publish to telem topic
+    if(radio_.available())
+    {
+      radio_.read(payload, PAYLOAD_SIZE);
+      interfaces::msg::TelemetryMsg msg;
+      msg.data = std::string(payload, payload + PAYLOAD_SIZE);
+      telem_publisher_->publish(msg);
+    }
   }
 
 } // namespace command_and_control
